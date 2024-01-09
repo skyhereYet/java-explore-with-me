d16761f0896d923816dc95eb7fdb558c380490fa
skyhereYet <mr.mvpopov@yandex.ru>
2024-01-09 22:13:40 +0300

    Feat: этап 1. Сервис статистики

--- main-service/src/main/java/ru/practikum/explore/MainServer.java
+++ main-service/src/main/java/ru/practikum/explore/MainServer.java
@@ -1,15 +1,12 @@
 package ru.practikum.explore;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class MainServer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
-        TestClient.testClient();
     }
 }
